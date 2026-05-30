8434d8d7aa2fd7240973cfa4e8ab4e9880be0c4f
Arup Debnath <arup.debnath@civica.com>
2026-05-30 13:38:16 +0530

    Apply variable.tf file

--- Lab3-Bastion Host Architecture/network.tf
+++ Lab3-Bastion Host Architecture/network.tf
@@ -1,26 +1,26 @@
 resource "aws_vpc" "lab03-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
 
   tags = {
-    Name = "lab03-vpc"
+    Name = "${var.env}-vpc"
   }
 }
 
 resource "aws_subnet" "lab03-pub-subnet" {
   vpc_id     = aws_vpc.lab03-vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "${var.public_subnet_cidr}"
 
   tags = {
-    Name = "lab03-pub-subnet"
+    Name = "${var.env}-pub-subnet"
   }
 }
 
 resource "aws_subnet" "lab03-pvt-subnet" {
   vpc_id     = aws_vpc.lab03-vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "${var.private_subnet_cidr}"
 
   tags = {
-    Name = "lab03-pvt-subnet"
+    Name = "${var.env}-pvt-subnet"
   }
 }
 
@@ -28,20 +28,20 @@ resource "aws_internet_gateway" "lab03-igw" {
   vpc_id = aws_vpc.lab03-vpc.id
 
   tags = {
-    Name = "lab03-igw"
+    Name = "${var.env}-igw"
   }
 }
 
 resource "aws_route_table" "lab03-rt" {
   vpc_id = aws_vpc.lab03-vpc.id
     tags = {
-        Name = "lab03-rt"
+        Name = "${var.env}-rt"
     }
 }
 
-resource "aws_route" "lab02-internet-route" {
+resource "aws_route" "lab03-internet-route" {
     route_table_id         = aws_route_table.lab03-rt.id
-    destination_cidr_block = "0.0.0.0/0"
+    destination_cidr_block = "${var.all_outbound}"
     gateway_id             = aws_internet_gateway.lab03-igw.id
 }
 
@@ -51,7 +51,7 @@ resource "aws_route_table_association" "lab03-pub-subnet-association" {
 }
 
 resource "aws_security_group" "lab03-sg" {
-  name        = "lab03-sg"
+  name        = "${var.env}-sg"
   description = "Allow SSH and ICMP traffic from the public subnet only"
   vpc_id      = aws_vpc.lab03-vpc.id
 
@@ -59,30 +59,30 @@ resource "aws_security_group" "lab03-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["${var.public_subnet_cidr}"]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["104.30.167.32/32"]
+    cidr_blocks = ["${var.my_ip}"]
   }
   ingress {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["${var.public_subnet_cidr}"]
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.all_outbound}"]
   }
 
     tags = {
-        Name = "lab03-sg"
+        Name = "${var.env}-sg"
     }
 
 }
