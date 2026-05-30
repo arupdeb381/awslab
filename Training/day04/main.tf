1cad457494a43801223bd80a93eee5eff1d451d2
Arup Debnath <arup.debnath@civica.com>
2026-05-30 23:31:38 +0530

    day04-04

--- Training/day04/main.tf
+++ Training/day04/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket = "arup-lab-aws-training"
+    key    = "day04/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -12,11 +21,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
+##################################################
+variable "env" {
+    default = "day04"
+    type = string
+}
+
+locals {
+  env = var.env
+  bucket_name = "training-arup-${local.env}"
+  vpc_name = "${var.env}-vpc"
+  pub_snet = "${var.env}-public-subnet"
+  pvt_snet = "${var.env}-private-subnet"
+  igw_name = "${var.env}-igw"
+  pub_rt_name = "${var.env}-public-rt"
+  sg_name = "${var.env}-sg"
+  ec2_instance_name = "${var.env}-ec2-instance"
+  
+}
+
 # Create a VPC
 resource "aws_vpc" "day04-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "day04-vpc"
+    env = var.env
+    Name = local.vpc_name
   }
 }
 
@@ -25,7 +54,8 @@ resource "aws_subnet" "day04-public-subnet" {
   vpc_id            = aws_vpc.day04-vpc.id
   cidr_block        = "10.0.1.0/24"
   tags = {
-    Name = "day04-public-subnet"
+    env = var.env
+    Name = local.pub_snet
   }
 }
 
@@ -33,20 +63,23 @@ resource "aws_subnet" "day04-private-subnet" {
   vpc_id            = aws_vpc.day04-vpc.id
   cidr_block        = "10.0.2.0/24"
   tags = {
-    Name = "day04-private-subnet"
+    env = var.env
+    Name = local.pvt_snet
   }
 }
 resource "aws_internet_gateway" "day04-igw" {
   vpc_id = aws_vpc.day04-vpc.id
   tags = {
-    Name = "day04-igw"
+    env = var.env
+    Name = local.igw_name
   }
 }
 
 resource "aws_route_table" "day04-public-rt" {
   vpc_id = aws_vpc.day04-vpc.id
   tags = {
-    Name = "day04-public-rt"
+    env = var.env
+    Name = local.pub_rt_name
   }
 }
 
@@ -62,7 +95,7 @@ resource "aws_route_table_association" "day04-public-rt-association" {
 }
 
 resource "aws_security_group" "day04-sg" {
-  name        = "day04-sg"
+  name        = local.sg_name
   description = "Allow SSH and HTTP traffic"
   vpc_id      = aws_vpc.day04-vpc.id
 
@@ -90,19 +123,20 @@ resource "aws_security_group" "day04-sg" {
 }
 
 # Create EC2 instaance
-resource "aws_instance" "public_ec2_instance" {
+resource "aws_instance" "ec2_instance_01" {
   ami           = "ami-00e801948462f718a" # Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.day04-public-subnet.id
+  subnet_id     = aws_subnet.day04-private-subnet.id
   security_groups = [aws_security_group.day04-sg.id]
   associate_public_ip_address = true
   key_name = "server01" # Replace with your actual key pair name
   tags = {
-    Name = "PublicEC2Instance"
+    env = var.env
+    Name = local.ec2_instance_name
   }
 }
 
 # Output the public IP of the public EC2 instance
 output "public_ec2_instance_public_ip" {
-  value = aws_instance.public_ec2_instance.public_ip
+  value = aws_instance.ec2_instance_01.public_ip
 }
