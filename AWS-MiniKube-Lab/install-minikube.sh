2494f17bdea649b8a519f3a830df660a1ce2f74e
Arup Debnath <arup.debnath@civica.com>
2026-06-06 13:39:30 +0530

    AWS minikube lab

---
+++ AWS-MiniKube-Lab/install-minikube.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+LOG_FILE="/home/ec2-user/minikube-install.log"
+
+exec > >(tee -a ${LOG_FILE})
+exec 2>&1
+
+set -e
+
+echo "========================================"
+echo "Installing Docker"
+echo "========================================"
+yum install -y docker
+
+systemctl enable docker
+systemctl start docker
+
+echo "========================================"
+echo "Adding ec2-user to docker group"
+echo "========================================"
+usermod -aG docker ec2-user
+
+echo "========================================"
+echo "Installing kubectl"
+echo "========================================"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s \
+https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
+
+echo "========================================"
+echo "Installing Minikube"
+echo "========================================"
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube version
+
+echo "========================================"
+echo "Starting Minikube"
+echo "========================================"
+
+sudo -u ec2-user /usr/local/bin/minikube start \
+  --driver=docker \
+  --cpus=2 \
+  --memory=3000mb
+
+echo "========================================"
+echo "Cluster Status"
+echo "========================================"
+
+sudo -u ec2-user /usr/local/bin/kubectl get nodes
+
+echo "========================================"
+echo "Minikube installation completed"
+echo "========================================"
---
+++ AWS-MiniKube-Lab/main.tf
@@ -0,0 +1,113 @@
+# Provider configuration for AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Backend configuration for Terraform state
+terraform {
+  backend "s3" {
+    bucket = "arup-lab-aws-training"
+    key    = "minikube/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
+}
+
+# tags for all resources
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Environment = "Lab"
+    Project     = "MiniKube"
+    Owner       = "Arup"
+  }
+}
+
+# VPC configuration
+resource "aws_vpc" "minikube_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags       = var.tags
+}
+
+# Subnet configuration
+resource "aws_subnet" "minikube_subnet" {
+  vpc_id            = aws_vpc.minikube_vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1a"
+  tags              = var.tags
+}
+
+# Internet Gateway configuration
+resource "aws_internet_gateway" "minikube_igw" {
+  vpc_id = aws_vpc.minikube_vpc.id
+  tags   = var.tags
+}
+
+# Route Table configuration
+resource "aws_route_table" "minikube_route_table" {
+  vpc_id = aws_vpc.minikube_vpc.id
+  tags   = var.tags
+}
+
+# Route configuration to allow internet access
+resource "aws_route" "minikube_route" {
+  route_table_id         = aws_route_table.minikube_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.minikube_igw.id
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "minikube_route_table_association" {
+  subnet_id      = aws_subnet.minikube_subnet.id
+  route_table_id = aws_route_table.minikube_route_table.id
+}
+
+# Security Group configuration
+resource "aws_security_group" "sg_minikube" {
+    name        = "minikube-sg"
+    description = "Security group for MiniKube lab"
+    vpc_id      = aws_vpc.minikube_vpc.id
+    tags        = var.tags
+    
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["104.30.167.32/32"] # Allow SSH from Arup's IP
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# EC2 Instance configuration for MiniKube
+resource "aws_instance" "minikube_instance" {
+    ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI
+    instance_type = "t2.medium"
+    subnet_id     = aws_subnet.minikube_subnet.id
+    security_groups = [aws_security_group.sg_minikube.id]
+    tags          = var.tags
+    associate_public_ip_address = true
+    key_name = "server01" # Replace with your actual key pair name
+    user_data = file("${path.module}/install-minikube.sh") # Script to install MiniKube on the EC2 instance
+    }
+
+# Output the public IP of the EC2 instance
+output "minikube_instance_public_ip" {
+  value = aws_instance.minikube_instance.public_ip
+}
